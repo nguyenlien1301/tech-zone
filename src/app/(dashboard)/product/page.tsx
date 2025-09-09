0c4894ea763e75da061c70334007b69404834ab6
Frondenddev <nguyenvanlien130102@gmail.com>
2025-09-10 02:01:58 +0700

    fix code

--- src/app/(dashboard)/product/page.tsx
+++ src/app/(dashboard)/product/page.tsx
@@ -1,13 +1,6 @@
 import { ProductPage } from "@/modules/product/pages";
-import { createUser } from "@/modules/user/actions";
 import Heading from "@/shared/components/common/heading";
 const ProductPageRoot = async () => {
-  const user = await createUser({
-    clerkId: "clerk_123",
-    email: "nguyenvanlien1234@gmail.com",
-    username: "nguyenlien",
-  });
-  console.log("🚀user---->", user);
   return (
     <>
       <Heading className="text-center py-10 bgDarkMode">Product</Heading>
